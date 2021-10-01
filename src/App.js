ed79a77b3ef70da5f7162988bdeeaf78bf85a0ed
shanawaz khan <shanawaz.khan@somaiya.edu>
2021-10-01 10:57:44 +0530

    stock world single Stock graph

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import Login from "./Components/Login";
+import "./App.css";
+import HomePage from "./Screens/HomePage";
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
